ba5fc136e41641b55b8722286d7ca51f0c59ea61
Singaravelu Souppourayen <34727666+sing-ideas@users.noreply.github.com>
2023-04-04 00:49:37 +0530

    changed to test github to local.

--- My13thApp/Program.cs
+++ My13thApp/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 //hope this 12th solutions is Commit Staged button works well.
+
+//i t is working well. 
