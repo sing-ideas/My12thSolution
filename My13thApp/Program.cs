a29ebad44f9919846086ff2444d3851fb7879e25
Singaravelu Souppourayen <34727666+sing-ideas@users.noreply.github.com>
2023-04-04 21:57:58 +0530

    2nd modification for out of sync repos

--- My13thApp/Program.cs
+++ My13thApp/Program.cs
@@ -6,3 +6,4 @@ Console.WriteLine("Hello, World!");
 // local to github using the sync (pull and push)
 
 // while local is having 2 outgoing in fetch details, I am modifying this file in the cloud before syncing from the local, expected result is to see the incoming is increased to 1
+// Unable to push from laptop to github after 1 modifcation in the github tab, this is the 2nd modification, I need to see the GUI representation of this out of sync repos.
